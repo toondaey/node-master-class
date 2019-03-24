8c5833cc55f537363ca26cd5682c7eaaf9691bd8
Tunde <tunde1990@gmail.com>
2019-03-24 07:17:05 +0100

    Assignment number two.

---
+++ config/index.js
@@ -0,0 +1,24 @@
+/**
+ * Configuration options for the server.
+ */
+// Dependencies
+
+// Determine environment.
+let currEnv = process.env.NODE_ENV, config;
+currEnv = (typeof currEnv === 'string' && currEnv || 'config').toLowerCase();
+
+try {
+    /**
+     * @description Get configuration based on environment
+     * @type {Object}
+     */
+    config = require(`./${currEnv}`);
+} catch (e) {
+    /**
+     * @default config
+     */
+    config = require('./config');
+}
+
+// Export module;
+module.exports = config;
